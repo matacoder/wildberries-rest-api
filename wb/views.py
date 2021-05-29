d8829f0897455a5942725907368b9caeb90adf00
Denis Matakov <denis@matakov.com>
2021-05-29 23:48:30 +0300

    Insane refactor of ordered/bought

--- wb/views.py
+++ wb/views.py
@@ -21,72 +21,55 @@ def index(request):
 
 @login_required
 def stock(request):
-    page_obj = []
-    try:
-        data = get_stock_products(user=request.user)
-        in_stock = filter(lambda x: x["quantity"] > 0, data)
-        sorted_in_stock = sorted(in_stock, key=lambda x: x["quantity"], reverse=True)
-        paginator = Paginator(sorted_in_stock, 32)
-        page_number = request.GET.get("page")
-        page_obj = paginator.get_page(page_number)
-    except Exception as e:
-        logging.warning(e)
+    data = get_stock_products(user=request.user)
+    in_stock = filter(lambda x: x["quantity"] > 0, data)
+    sorted_in_stock = sorted(in_stock, key=lambda x: x["quantity"], reverse=True)
+    paginator = Paginator(sorted_in_stock, 32)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    data = get_info_widget(request.user)
+    data["data"] = page_obj
     return render(
         request,
         "stock.html",
-        {
-            "data": page_obj,
-            "payment": get_weekly_payment(user=request.user),
-            "ordered": get_ordered_sum(user=request.user),
-            "bought": get_bought_sum(user=request.user),
-        },
+        data,
     )
+
+
+def get_info_widget(user):
+    return {
+        "payment": get_weekly_payment(user=user),
+        "ordered": get_ordered_sum(user=user),
+        "bought": get_bought_sum(user=user),
+    }
 
 
 @login_required
 def ordered(request):
-    page_obj = []
-    try:
-        data = get_ordered_products(user=request.user)
-        sorted_by_date = sorted(data, key=lambda x: x["date"], reverse=True)
-        paginator = Paginator(sorted_by_date, 32)
-        page_number = request.GET.get("page")
-        page_obj = paginator.get_page(page_number)
-    except Exception as e:
-        logging.warning(e)
+    return render_page(get_ordered_products, request)
+
+
+def render_page(function, request):
+    data = function(user=request.user)
+    sorted_by_date = sorted(data, key=lambda x: x["date"], reverse=True)
+    paginator = Paginator(sorted_by_date, 32)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    data = get_info_widget(request.user)
+    data["data"] = page_obj
+
     return render(
         request,
         "ordered.html",
-        {
-            "data": page_obj,
-            "payment": get_weekly_payment(user=request.user),
-            "ordered": get_ordered_sum(user=request.user),
-            "bought": get_bought_sum(user=request.user),
-        },
+        data,
     )
 
 
 @login_required
 def bought(request):
-    page_obj = []
-    try:
-        data = get_bought_products(user=request.user)
-        sorted_by_date = sorted(data, key=lambda x: x["date"], reverse=True)
-        paginator = Paginator(sorted_by_date, 32)
-        page_number = request.GET.get("page")
-        page_obj = paginator.get_page(page_number)
-    except Exception as e:
-        logging.warning(e)
-    return render(
-        request,
-        "ordered.html",
-        {
-            "data": page_obj,
-            "payment": get_weekly_payment(user=request.user),
-            "ordered": get_ordered_sum(user=request.user),
-            "bought": get_bought_sum(user=request.user),
-        },
-    )
+    return render_page(get_bought_products, request)
 
 
 @login_required
